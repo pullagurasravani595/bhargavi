f0808316b3a4eb5296901b2c056c9b66356bb89a
sravani <pullagurasravani595@gmail.com>
2024-01-31 23:05:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Contact from './component/Contact'
+import Header from './component/Header'
+
+const app = () => (
+  <div>
+    <Contact />
+    <Header />
+  </div>
+)
+
+export default app
---
+++ src/component/Header/index.js
@@ -0,0 +1,9 @@
+
+const Header = () => (
+    <div>
+        <h1>Header</h1>
+    </div>
+)
+
+export default Header 
+
